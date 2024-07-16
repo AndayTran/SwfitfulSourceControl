152975b4a70e102c6f20edb7170955eea0a9b21a
Andy <thienkhoatran@gmail.com>
2024-07-15 18:12:13 -0700

    Initial Commit

---
+++ SwfitfulSourceControl/SwfitfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitfulSourceControlApp.swift
+//  SwfitfulSourceControl
+//
+//  Created by Khoa Tran on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwfitfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
